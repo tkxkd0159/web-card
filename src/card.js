c5d0efd71ef38b75cdb193cc2346f2277a4a0275
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-12-21 15:09:34 +0900

    block mobile device

--- src/card.js
+++ src/card.js
@@ -1,6 +1,20 @@
 const letterClose = document.querySelector(".letter-close");
 const letterOpen = document.querySelector(".letter-open");
 
+function isMobile() {
+  if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)) {
+    return true;
+  }
+
+  return false;
+}
+
 function close_state() {
   document.querySelector("#envelop").addEventListener("click", () => {
     if (letterClose.classList.contains("appear")) {
